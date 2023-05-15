76800a4cec1e6c24f1a23b48cec28cc48bf00802
saway11 <rgarcia20.m@gmail.com>
2023-05-14 19:07:57 -0700

    added variables

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,6 @@
+const $noteTitle = $(".note-title");
+const $noteText = $(".note-textarea");
+const $saveNotebtn = $(".save-note");
+const $newNoteBtn = $(".new-note");
+const $noteList = $(".list-container .list-group");
 
