b0e9f8287b1f4eae5c0291d5cddc3ae90fc8aa72
saway11 <rgarcia20.m@gmail.com>
2023-05-14 19:10:49 -0700

    activeNote to keep trakc of the note, added function to get all notes from the db

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,6 +1,18 @@
+// Variables
 const $noteTitle = $(".note-title");
 const $noteText = $(".note-textarea");
 const $saveNotebtn = $(".save-note");
 const $newNoteBtn = $(".new-note");
 const $noteList = $(".list-container .list-group");
+
+// activeNote is used to keep track of the note in the textarea
+const activeNote = {};
+
+// A function for getting all notes from the db
+const getNotes = function() {
+    return $.ajax({
+        url: "/api/notes",
+        method: "GET"
+    });
+};
 
